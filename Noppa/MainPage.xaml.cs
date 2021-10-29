49a5e46d4a1defa7164caf13214d156535ec55b5
Simo Siren <sirensimo7@gmail.com>
2021-10-29 10:31:47 +0300

    Add project files.

---
+++ Noppa/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Noppa
+{
+    public partial class MainPage : ContentPage
+    {
+        Random rnd = new Random();
+        string setting = "6";
+
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (setting == "6")
+            {
+                numeroLabel.Text = rnd.Next(1, 7).ToString();
+            }
+            else
+            {
+                numeroLabel.Text = rnd.Next(1, 13).ToString();
+            }
+        }
+
+        private void btn6_Clicked(object sender, EventArgs e)
+        {
+            setting = "6";
+            btn12.BackgroundColor = Color.White;
+            btn6.BackgroundColor = Color.LightGreen;
+        }
+
+        private void btn12_Clicked(object sender, EventArgs e)
+        {
+            setting = "12";
+            btn12.BackgroundColor = Color.LightGreen;
+            btn6.BackgroundColor = Color.White;
+        }
+    }
+}
